84ba3312d3249833014d452831e22c33dafd4941
crestel-ong <crestel.ong@mths.ca>
2021-09-13 23:44:31 +0000

    good version

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Crestel Ong
+# Created on: Sept 2021
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+
+    print(9 + 2)
+    print("7-3={}".format(7 - 3))
+    print("4/2={}".format(4 / 2))
+    print("4+4/2={}".format(4 + 4 / 2))
+    print("3+4*2={}".format(3 + 4 * 2))
+    print("5+2^3={}".format(5 + 2 ^ 3))  # Bitwise Operators
+    print("5+2³={}".format(5 + 2 ** 3))  # exponent
+
+
+if __name__ == "__main__":
+    main()
